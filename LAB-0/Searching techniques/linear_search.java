a5057099c63d4f97964d5bb8ec59dfe9fc9a6120
Siva Ganesh Pepakayala <157547445+sivaganesh7@users.noreply.github.com>
2024-12-31 11:33:23 +0530

    Update linear_search.java

--- LAB-0/Searching techniques/linear_search.java
+++ LAB-0/Searching techniques/linear_search.java
@@ -1,28 +1,35 @@
-
 import java.util.Scanner;
-
-public class linear_search{
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        System.out.println("enter the size of array: ");
-        int n=s.nextInt();
-        int[] a=new int[n];
-        System.out.println("enter the elements of array: ");
-        for(int i=0;i<a.length;i++){
-            a[i]=s.nextInt();
-        }
-        System.out.println("enter the element to search");
-        int key=s.nextInt();
-        boolean f=false;
-        for (int i =0; i < n; i++) {
-            if(a[i]==key){
-                System.out.println("element found at index "+i);
-                f=true;
-                
-            }
-        }
-        if(f==false){
-            System.out.println("element not found");
-        }
-    }
+public class lsearch {
+	public static void main(String args[]){
+		Scanner read = new Scanner(System.in);
+		
+		System.out.print("Enter the no. of elements: ");
+		int n = read.nextInt();
+		int[] arr = new int[n];
+		
+		System.out.println("Enter the elements: ");
+		for(int i = 0; i < n; i++){
+			arr[i] = read.nextInt();
+		}
+		
+		System.out.print("Enter the key: ");
+		int key = read.nextInt();
+		boolean pos = linearSearch(arr, key);		
+		if (pos != false){
+			System.out.println("Key is found at position " + pos);
+		}
+		else {
+			System.out.println("Key is not found");
+		}
+	}
+	static boolean linearSearch(int a[], int k){
+		boolean f=false;
+		for(int i = 0; i < a.length; i++){
+			if(k == a[i]){
+				f=true;
+				break;
+			}
+		}
+		return f;
+	}
 }
