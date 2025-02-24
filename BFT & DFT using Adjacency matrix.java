44ae960f0d4678d4f3656e4d0a4612cde6125176
Siva Ganesh Pepakayala <157547445+sivaganesh7@users.noreply.github.com>
2025-02-24 21:13:18 +0530

    Update and rename BFS & DFS using Adjacency matrix.java to BFT & DFT using Adjacency matrix.java

--- BFT & DFT using Adjacency matrix.java
+++ BFT & DFT using Adjacency matrix.java
@@ -1,51 +1,56 @@
 import java.util.*;
 
-public class GraphMatrix {
-    private int[][] adjacencyMatrix;
+class GraphMatrix {
+    private int[][] adjMatrix;
     private int vertices;
 
     public GraphMatrix(int vertices) {
         this.vertices = vertices;
-        adjacencyMatrix = new int[vertices][vertices];
+        adjMatrix = new int[vertices][vertices];
     }
 
     public void addEdge(int src, int dest) {
-        adjacencyMatrix[src][dest] = 1;
-        adjacencyMatrix[dest][src] = 1; // For an undirected graph
+        adjMatrix[src][dest] = 1;
+        adjMatrix[dest][src] = 1; // Assuming an undirected graph
     }
 
     public void bfs(int start) {
         boolean[] visited = new boolean[vertices];
         Queue<Integer> queue = new LinkedList<>();
         
-        visited[start] = true;
         queue.add(start);
+        visited[start] = true;
+
+        System.out.print("BFT (Adjacency Matrix): ");
         
         while (!queue.isEmpty()) {
             int node = queue.poll();
             System.out.print(node + " ");
 
             for (int i = 0; i < vertices; i++) {
-                if (adjacencyMatrix[node][i] == 1 && !visited[i]) {
+                if (adjMatrix[node][i] == 1 && !visited[i]) {
                     queue.add(i);
                     visited[i] = true;
                 }
             }
         }
+        System.out.println();
     }
 
     public void dfs(int start) {
         boolean[] visited = new boolean[vertices];
-        dfsUtil(start, visited);
+        System.out.print("DFT (Adjacency Matrix): ");
+        dfsRecursive(start, visited);
+        System.out.println();
     }
 
-    private void dfsUtil(int node, boolean[] visited) {
+    private void dfsRecursive(int node, boolean[] visited) {
         visited[node] = true;
         System.out.print(node + " ");
 
         for (int i = 0; i < vertices; i++) {
-            if (adjacencyMatrix[node][i] == 1 && !visited[i]) {
-                dfsUtil(i, visited);
+            if (adjMatrix[node][i] == 1 && !visited[i]) {
+                dfsRecursive(i, visited);
             }
         }
     }
@@ -56,11 +61,9 @@ public class GraphMatrix {
         graph.addEdge(0, 2);
         graph.addEdge(1, 3);
         graph.addEdge(1, 4);
-        
-        System.out.println("BFS Traversal:");
+        graph.addEdge(2, 4);
+
         graph.bfs(0);
-        
-        System.out.println("\nDFS Traversal:");
         graph.dfs(0);
     }
 }
