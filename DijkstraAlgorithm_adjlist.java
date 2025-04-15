8c6878bcb1173c7a2d9853173097b0ba12bedabb
Siva Ganesh Pepakayala <157547445+sivaganesh7@users.noreply.github.com>
2025-04-15 22:34:51 +0530

    Update DijkstraAlgorithm_adjlist.java

--- DijkstraAlgorithm_adjlist.java
+++ DijkstraAlgorithm_adjlist.java
@@ -60,20 +60,12 @@ class DijkstraAlgorithm {
         int V = 9;
         Graph graph = new Graph(V);
 
-        graph.addEdge(0, 1, 4);
-        graph.addEdge(0, 7, 8);
-        graph.addEdge(1, 2, 8);
-        graph.addEdge(1, 7, 11);
-        graph.addEdge(2, 3, 7);
-        graph.addEdge(2, 8, 2);
-        graph.addEdge(2, 5, 4);
-        graph.addEdge(3, 4, 9);
-        graph.addEdge(3, 5, 14);
-        graph.addEdge(4, 5, 10);
-        graph.addEdge(5, 6, 2);
-        graph.addEdge(6, 7, 1);
-        graph.addEdge(6, 8, 6);
-        graph.addEdge(7, 8, 7);
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 1);
+        graph.addEdge(1, 3, 2);
+        graph.addEdge(1, 4, 3);
+        graph.addEdge(2, 4, 4);
+
 
         int source = 0;
         graph.dijkstra(source);
